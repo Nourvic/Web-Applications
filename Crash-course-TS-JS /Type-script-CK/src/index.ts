3ccb58f4b0dd67095d0b153deb502a1924488b03
Nourvic <nourvic7@gmail.com>
2025-09-27 15:11:57 +0200

    "test test"

---
+++ Crash-course-TS-JS /Type-script-CK/src/index.ts
@@ -0,0 +1,8 @@
+let userid:number = 25;
+var firstname:string = "Nour";
+console.log("ID :" + userid + " and name is : " + firstname);
+let str:string = "Hallo Nour";
+alert(str);
+
+let str2:string = "SaY my NaMe";
+alert(str2);
