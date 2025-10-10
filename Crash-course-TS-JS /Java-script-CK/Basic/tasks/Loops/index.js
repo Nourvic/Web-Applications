e252a6b03818a7b0c4cf56fe6b7a571a227a7913
Nourvic <nourvic7@gmail.com>
2025-10-11 01:52:31 +0200

    some changes

--- Crash-course-TS-JS /Java-script-CK/Basic/tasks/Loops/index.js
+++ Crash-course-TS-JS /Java-script-CK/Basic/tasks/Loops/index.js
@@ -1,3 +1,23 @@
 var students = ["Nour", "Roun", "Ounr", "Unor"];
-document.getElementById("students").innerHTML = students;
-ƒ;
+//document.getElementById("students").innerHTML = students;
+
+// for-math
+for (var i = 0; i <= 30; i++) {
+  //  console.log(` 10 x ${i} = ${10 * i}`);
+}
+
+//for string
+var nam = "Nour";
+var test = "";
+for (let i = 0; i < nam.length; i++) {
+  test += nam[i] + " ";
+}
+console.log(test);
+//for-advanced { for in }
+for (var i in nam) {
+  console.log(nam[i]);
+}
+// for of
+for (var i of nam) {
+  console.log(i);
+}
