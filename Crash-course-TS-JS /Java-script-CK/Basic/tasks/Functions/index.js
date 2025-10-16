9451eee2fbd2f20ac700f9d6ae88915fc1764c02
Nourvic <nourvic7@gmail.com>
2025-10-17 00:26:37 +0200

    k

---
+++ Crash-course-TS-JS /Java-script-CK/Basic/tasks/Functions/index.js
@@ -0,0 +1,9 @@
+function printwelcome() {
+  console.log("Hello \nWelcome \nHow Are you ?");
+}
+printwelcome();
+
+function printwelcomeper(name) {
+  console.log(`Hello ${name}`);
+}
+printwelcomeper("Nour");
