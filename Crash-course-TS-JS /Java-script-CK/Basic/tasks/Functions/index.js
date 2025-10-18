fdb88c97a8b2556d1ac90d65c32eacc651283d4a
Nourvic <nourvic7@gmail.com>
2025-10-18 16:07:11 +0200

    add functions

--- Crash-course-TS-JS /Java-script-CK/Basic/tasks/Functions/index.js
+++ Crash-course-TS-JS /Java-script-CK/Basic/tasks/Functions/index.js
@@ -7,3 +7,14 @@ function printwelcomeper(name) {
   console.log(`Hello ${name}`);
 }
 printwelcomeper("Nour");
+
+function reverseArray(arr = []) {
+  const x = [];
+  for (elem of arr) {
+    x.unshift(elem);
+  }
+  return x;
+}
+var myArray = [10, "Hello", 50, 70, true, false, "c"];
+var y = reverseArray(myArray);
+console.log(y);
