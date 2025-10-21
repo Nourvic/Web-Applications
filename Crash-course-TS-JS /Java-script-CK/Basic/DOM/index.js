ca8ad30d4399be435519701fe05e5f05e9407b22
Nourvic <nourvic7@gmail.com>
2025-10-21 23:22:10 +0200

    test dom

---
+++ Crash-course-TS-JS /Java-script-CK/Basic/DOM/index.js
@@ -0,0 +1,11 @@
+let x = "hi";
+let y = x;
+//pass by Value
+console.log("The x value is: " + x);
+console.log("The y value is: " + y);
+let w = ["hello"];
+let z = w;
+w.push("Hi");
+console.log("The w value is: " + w);
+console.log("The z value is: " + z);
+
