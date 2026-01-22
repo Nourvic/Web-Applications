add5926d2cc663ea46d3d1aa9f3691ea3afb1dd5
Nourvic <nourvic7@gmail.com>
2026-01-22 21:48:01 +0100

    fixed a bug

--- Crash-course-TS-JS /Java-script-CK/Basic/tasks/task6/index.js
+++ Crash-course-TS-JS /Java-script-CK/Basic/tasks/task6/index.js
@@ -5,11 +5,11 @@ Was sind x und y?
 x → Zwischenergebnis (Summe bis jetzt)
 y → aktuelles Element aus dem Array
 */
-
+// x+y , 0 -> 0 ist Startwert
 function cumulativeAddition(array) {
   let myarr = array.reduce((x, y) => {
-    return x + y;
-  });
+    return x+y
+  },100);
   console.log(`Sum is :${myarr} and Elements of are ${array.length}`);
 }
 cumulativeAddition(arr);
