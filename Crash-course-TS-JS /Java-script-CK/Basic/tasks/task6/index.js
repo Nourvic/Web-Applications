c1b74286ccdb289ff992e81ba24c80f7f2da5c2c
Nourvic <nourvic7@gmail.com>
2026-01-22 21:43:10 +0100

    fixed a bug

--- Crash-course-TS-JS /Java-script-CK/Basic/tasks/task6/index.js
+++ Crash-course-TS-JS /Java-script-CK/Basic/tasks/task6/index.js
@@ -1,4 +1,10 @@
 var arr = [50, 50, 50, 50, 50];
+/*
+👉 reduce geht von links nach rechts durchs Array.
+Was sind x und y?
+x → Zwischenergebnis (Summe bis jetzt)
+y → aktuelles Element aus dem Array
+*/
 
 function cumulativeAddition(array) {
   let myarr = array.reduce((x, y) => {
