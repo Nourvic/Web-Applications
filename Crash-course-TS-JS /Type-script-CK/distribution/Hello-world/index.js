c3be822efe3e14e5d3a824ec33634d162207cfaa
Nourvic <nourvic7@gmail.com>
2026-01-24 16:46:00 +0100

    test

--- Crash-course-TS-JS /Type-script-CK/distribution/Hello-world/index.js
+++ Crash-course-TS-JS /Type-script-CK/distribution/Hello-world/index.js
@@ -1,10 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let x = 10;
-let y = "10";
+var x = 10;
+var y = "10";
 y = "Hello zusammen";
 y = document.getElementById("first").innerHTML = "Hello World from Typescript";
 alert(y);
 alert("Hii");
 alert("hey");
-//# sourceMappingURL=index.js.map
