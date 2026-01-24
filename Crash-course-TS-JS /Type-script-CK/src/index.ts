223d808eef647bdb604886be1a320de08d929f35
Nourvic <nourvic7@gmail.com>
2026-01-24 16:14:53 +0100

    remove

--- Crash-course-TS-JS /Type-script-CK/src/index.ts
+++
@@ -1,8 +0,0 @@
-let userid:number = 25;
-var firstname:string = "Nour";
-console.log("ID :" + userid + " and name is : " + firstname);
-let str:string = "Hallo Nour";
-alert(str);
-
-let str2:string = "SaY my NaMe";
-alert(str2);
