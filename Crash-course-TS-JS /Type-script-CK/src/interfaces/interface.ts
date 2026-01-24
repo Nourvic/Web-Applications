fc184ab8f995c56fcd5e71012d7b7153c760b73b
Nourvic <nourvic7@gmail.com>
2026-01-24 20:46:57 +0100

    interface is finished

---
+++ Crash-course-TS-JS /Type-script-CK/src/interfaces/interface.ts
@@ -0,0 +1,29 @@
+export interface Person {
+  name: string;
+  age?:number;
+}
+let y: Person;
+
+y = {
+  name: "",
+  age: 25,
+};
+
+let test: Person = {
+  name: "nour",
+};
+
+type myType = string | number;
+type myType2 = {
+  name: string;
+  age: number;
+};
+
+let w: myType2 = {
+  name: "nour",
+  age: 25,
+};
+
+const myNumber1:number = y.age!;
+const myNumber2:number = y.age ?? 0;
+
