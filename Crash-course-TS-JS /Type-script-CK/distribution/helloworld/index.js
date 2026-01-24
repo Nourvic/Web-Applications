0b4c38aaad8bcd320d7c32620ab23cff2b5eecda
Nourvic <nourvic7@gmail.com>
2026-01-24 20:47:50 +0100

    interfaces is finish

---
+++ Crash-course-TS-JS /Type-script-CK/distribution/helloworld/index.js
@@ -0,0 +1,5 @@
+var x = 20;
+var y = "10";
+y = "Hello zusammen";
+y = document.getElementById("first").innerHTML = "Hello World from Typescript";
+alert(y);
---
+++ Crash-course-TS-JS /Type-script-CK/distribution/interfaces/interface.js
@@ -0,0 +1,17 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+var y;
+y = {
+    name: "",
+    age: 25,
+};
+var test = {
+    name: "nour",
+};
+var w = {
+    name: "nour",
+    age: 25,
+};
+var myNumber1 = y.age;
+var myNumber2 = (_a = y.age) !== null && _a !== void 0 ? _a : 0;
