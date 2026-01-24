68d9adb6c7bf8cfeabdd110280226f615ee8c471
Nourvic <nourvic7@gmail.com>
2026-01-24 16:48:05 +0100

    test

--- Crash-course-TS-JS /Type-script-CK/src/Hello-world/index.ts
+++ Crash-course-TS-JS /Type-script-CK/src/Hello-world/index.ts
@@ -1,10 +1,5 @@
 let x = 10;
-
 let y:string ="10";
 y = "Hello zusammen";
-
 y = document.getElementById("first")!.innerHTML = "Hello World from Typescript";
-
 alert(y);
-alert("Hii")
-alert("hey")
