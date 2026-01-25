8577bd9025d8ec8a235779f5130877b37064ac46
Nourvic <nourvic7@gmail.com>
2026-01-25 22:40:14 +0100

    adding some style

--- React-with-ts/react-project/src/myComponent/Fstep.tsx
+++ React-with-ts/react-project/src/myComponent/Fstep.tsx
@@ -1,9 +1,28 @@
+import "./Fstep.css";
+
 export default function Fstep() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <h2>hi</h2>
-      <button>Click Me</button>
+      <h1 style={elmStyle}> Hey from React</h1>
+      <h2 style={{ backgroundColor: "tan" }}>
+        hi {person.name} , email : {person.email}
+      </h2>
+      <button className="button" onClick={sayHello}>
+        Click Me
+      </button>
     </div>
   );
-} 
+}
+
+const elmStyle = {
+  backgroundColor: "red",
+};
+
+const sayHello = () => {
+  alert(`hi ${person.name} what can i do for u today ?`);
+};
+
+const person = {
+  name: "Nour",
+  email: "test@gmail.com",
+};
