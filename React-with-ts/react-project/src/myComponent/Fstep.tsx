d893522655547357bffc1b91d7d96e1473be2d30
Nourvic <nourvic7@gmail.com>
2026-01-25 21:27:52 +0100

    first component

--- React-with-ts/react-project/src/myComponent/Fstep.tsx
+++ React-with-ts/react-project/src/myComponent/Fstep.tsx
@@ -1,3 +1,9 @@
-export default function fstep() {
-  return <div>my-Component</div>;
-}
+export default function Fstep() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <h2>hi</h2>
+      <button>Click Me</button>
+    </div>
+  );
+} 
