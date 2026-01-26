e5e0c805b69d187d159cb2fa9f683e5e94288f88
Nourvic <nourvic7@gmail.com>
2026-01-26 22:24:05 +0100

    task3 with useState is done

--- React-with-ts/react-project/src/myComponent/task3/MyButton.tsx
+++ React-with-ts/react-project/src/myComponent/task3/MyButton.tsx
@@ -1,12 +1,29 @@
-let name = "Nour";
+import { useState } from "react";
+
 export default function MyButton() {
+  /*
+  console.log("render");
+  const state = useState(name2);
+  const value = state[0];
+  console.log(value);
+  const setValue = state[1];
+*/
+  const [name, setName] = useState("test");
+
+  function buttonClicked() {
+    /*
+    if (name == "test") {
+      setName("Nour");
+    } else {
+      setName("test");
+    }
+    */
+    return setName(name == "test" ? "Nour" : "test");
+  }
   return (
     <div>
       <button onClick={buttonClicked}> Click Me</button>
       <h3>{name}</h3>
     </div>
   );
-}
-function buttonClicked() {
-  name = "asd";
 }
