188961f1adc326e0bf0e678108e3300b35f590ce
Nourvic <nourvic7@gmail.com>
2026-01-26 20:08:34 +0100

    task 2 is ready

--- React-with-ts/react-project/src/App.tsx
+++ React-with-ts/react-project/src/App.tsx
@@ -6,32 +6,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 //import "./myComponent/Fstep";
 //import Fstep from "./myComponent/Fstep";
-import Header from "./myComponent/task1/Header";
-import Posts from "./myComponent/task1/Posts";
-import SideMenu from "./myComponent/task1/SideMenu";
+//import Header from "./myComponent/task1/Header";
+//import Posts from "./myComponent/task1/Posts";
+//import SideMenu from "./myComponent/task1/SideMenu";
+
 function App() {
   return (
     <>
-      <div>
-        <Header />
-        <div style={{ width: "100%" }}>
-          <div style={{ display: "flex" }}>
-            {/*Posts container */}
-            <div style={{ width: "70%" }}>
-              <Posts />
-              <Posts />
-              <Posts />
-            </div>
-            {/*Posts container */}
-
-            {/*SideMenu container */}
-            <div>
-              <SideMenu />
-            </div>
-          </div>
-          {/*SideMenu container */}
-        </div>
-      </div>
+      <div></div>
     </>
   );
 }
---
+++ React-with-ts/react-project/src/myComponent/task2/artikel.tsx
@@ -0,0 +1,14 @@
+interface person {
+  name: string;
+  email: string;
+  age: number;
+}
+export default function artikel(props) {
+  return (
+    <div>
+      <h1>Roun</h1>
+      <h3>test@gmail.com</h3>
+      <h3>1999</h3>
+    </div>
+  );
+}
